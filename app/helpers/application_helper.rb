9cb486d439eb62a2ad8998694797cb185b4ea55d
Jeremie Delaitre <jeremie.delaitre@technogerma.fr>
2010-05-04 14:39:29 +0200

    Display the "Not running" entry if the user has the log_time right in one project at least (issue #12)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,18 @@ module ApplicationHelper
     def time_tracker_for(user)
         TimeTracker.find(:first, :conditions => { :user_id => user.id })
     end
+
+    def global_allowed_to?(user, action)
+        return false if user.nil?
+
+        projects = Project.find(:all)
+        for p in projects
+            if user.allowed_to?(action, p)
+                return true
+            end
+        end
+
+        return false
+    end
 end
 
