9ac9507b610b7a617853dec6e1d032c3373c61cf
MischaTheEvil <mischa_the_evil@hotmail.com>
2010-05-15 05:07:34 +0200

    Added i18n'ed journal-note when a status transition is applied

--- app/controllers/time_trackers_controller.rb
+++ app/controllers/time_trackers_controller.rb
@@ -77,7 +77,7 @@ class TimeTrackersController < ApplicationController
         new_status_id = Setting.plugin_redmine_time_tracker['status_transitions'][issue.status_id.to_s]
         new_status = IssueStatus.find(:first, :conditions => { :id => new_status_id })
         if issue.new_statuses_allowed_to(User.current).include?(new_status)
-            journal = @issue.init_journal(User.current)
+            journal = @issue.init_journal(User.current, notes = l(:time_tracker_label_transition_journal))
             @issue.status_id = new_status_id
             @issue.save
         end
