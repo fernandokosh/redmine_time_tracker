082ca339abf3706ea844622e147f89f1b8fee71f
jcopp <jcopp@sportingnews.com>
2012-04-05 02:34:16 -0400

    Add routes for settings page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ ActionController::Routing::Routes.draw do |map|
   map.connect '/time_trackers/suspend', :controller => 'time_trackers', :action => 'suspend'
   map.connect '/time_trackers/resume', :controller => 'time_trackers', :action => 'resume'
   map.connect '/time_trackers/render_menu', :controller => 'time_trackers', :action => 'render_menu'
+  map.connect '/time_trackers/add_status_transition', :controller => 'time_trackers', :action => 'add_status_transition'
+  map.connect '/time_trackers/delete_status_transition', :controller => 'time_trackers', :action => 'delete_status_transition'
   map.connect '/time_trackers/show_report', :controller => 'time_trackers', :action => 'show_report'
   map.connect '/time_trackers/delete', :controller => 'time_trackers', :action => 'delete'
   map.connect '/time_trackers', :controller => 'time_trackers', :action => 'index'
