b857397c33d4af6fc1f38e8533b2d642ef75b414
Christian Reich <christian.reich@hicknhack-software.com>
2012-05-19 13:27:54 +0200

    ported redmine_time_tracker to run on redmine2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect '/time_trackers/stop', :controller => 'time_trackers', :action => 'stop'
-  map.connect '/time_trackers/start', :controller => 'time_trackers', :action => 'start'
-  map.connect '/time_trackers/suspend', :controller => 'time_trackers', :action => 'suspend'
-  map.connect '/time_trackers/resume', :controller => 'time_trackers', :action => 'resume'
-  map.connect '/time_trackers/render_menu', :controller => 'time_trackers', :action => 'render_menu'
-  map.connect '/time_trackers/add_status_transition', :controller => 'time_trackers', :action => 'add_status_transition'
-  map.connect '/time_trackers/delete_status_transition', :controller => 'time_trackers', :action => 'delete_status_transition'
-  map.connect '/time_trackers/show_report', :controller => 'time_trackers', :action => 'show_report'
-  map.connect '/time_trackers/delete', :controller => 'time_trackers', :action => 'delete'
-  map.connect '/time_trackers', :controller => 'time_trackers', :action => 'index'
-end
+match '/time_trackers/stop', :to => 'time_trackers#stop', :via => [:get, :post]
+match '/time_trackers/start', :to => 'time_trackers#start', :via => [:get, :post]
+match '/time_trackers/suspend', :to => 'time_trackers#suspend', :via => [:get, :post]
+match '/time_trackers/resume', :to => 'time_trackers#resume', :via => [:get, :post]
+match '/time_trackers/render_menu', :to => 'time_trackers#render_menu', :via => [:get, :post]
+match '/time_trackers/add_status_transition', :to => 'time_trackers#add_status_transition', :via => [:get, :post]
+match '/time_trackers/delete_status_transition', :to => 'time_trackers#delete_status_transition', :via => [:get, :post]
+match '/time_trackers/show_report', :to => 'time_trackers#show_report', :via => [:get, :post]
+match '/time_trackers/delete', :to => 'time_trackers#delete', :via => [:get, :post]
+match '/time_trackers', :to => 'time_trackers#index', :via => [:get, :post]
--- init.rb
+++ init.rb
@@ -1,26 +1,32 @@
+# encoding: utf-8
+Rails.logger.info 'Starting Time Tracker plugin for RedMine'
+
 require 'redmine'
+
+# workaround helping rails to find the helper-methods
+require File.join(File.dirname(__FILE__), "app", "helpers", "application_helper.rb")
 
 require_dependency 'time_tracker_hooks'
 
 Redmine::Plugin.register :redmine_time_tracker do
-    name 'Redmine Time Tracker plugin'
-    author 'Jérémie Delaitre'
-    description 'This is a plugin to track time in Redmine'
-    version '0.4'
+  name 'Redmine Time Tracker plugin'
+  author 'Jérémie Delaitre'
+  description 'This is a plugin to track time in Redmine'
+  version '0.4'
 
-    requires_redmine :version_or_higher => '1.1.0'
+  requires_redmine :version_or_higher => '2.0.0'
 
-    settings :default => { 'refresh_rate' => '60', 'status_transitions' => {} }, :partial => 'settings/time_tracker'
+  settings :default => { 'refresh_rate' => '60', 'status_transitions' => {} }, :partial => 'settings/time_tracker'
 
-    permission :view_others_time_trackers, :time_trackers => :index
-    permission :delete_others_time_trackers, :time_trackers => :delete
+  permission :view_others_time_trackers, :time_trackers => :index
+  permission :delete_others_time_trackers, :time_trackers => :delete
 
-    menu :account_menu, :time_tracker_menu, '',
-        {
-            :caption => '',
-            :html => { :id => 'time-tracker-menu' },
-            :first => true,
-            :param => :project_id,
-            :if => Proc.new { User.current.logged? }
-        }
+  menu :account_menu, :time_tracker_menu, '',
+       {
+           :caption => '',
+           :html => { :id => 'time-tracker-menu' },
+           :first => true,
+           :param => :project_id,
+           :if => Proc.new { User.current.logged? }
+       }
 end
