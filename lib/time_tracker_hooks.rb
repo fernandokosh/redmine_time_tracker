9af257d8b474ddba6480e0f5c4982140d1fc9624
Christian Reich <christian.reich@hicknhack-software.com>
2012-06-19 16:51:36 +0200

    cleaned up (removed unused files)

--- lib/time_tracker_hooks.rb
+++ lib/time_tracker_hooks.rb
@@ -1,6 +1,5 @@
 # This class hooks into Redmine's View Listeners in order to add content to the page
 class TimeTrackerHooks < Redmine::Hook::ViewListener
-  #render_on :view_layouts_base_body_bottom, :partial => 'time_trackers/update_menu'
   render_on :view_issues_context_menu_start, :partial => 'time_trackers/update_context'
 
   def view_layouts_base_html_head(context = {})
