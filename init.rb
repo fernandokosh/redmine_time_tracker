4ed00bbc588bee9fbebe22458b7fa57785fa9cdc
Thomas von Deyen <tvd@magiclabs.de>
2012-06-26 23:14:21 +0200

    Bumped version to 0.5

--- init.rb
+++ init.rb
@@ -6,9 +6,9 @@ Redmine::Plugin.register :chiliproject_time_tracker do
     name 'ChiliProject Time Tracker plugin'
     author 'Jérémie Delaitre, magic labs*'
     description 'This is a plugin to track time in ChiliProject'
-    version '0.4'
+    version '0.5'
 
-    requires_redmine :version_or_higher => '1.1.0'
+    requires_redmine :version_or_higher => '3.1.0'
 
     settings :default => { 'refresh_rate' => '60', 'status_transitions' => {} }, :partial => 'settings/time_tracker'
 
