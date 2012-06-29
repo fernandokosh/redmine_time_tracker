c9db91aa2c91143c753e4edfde25229bdac9edb5
Christian Reich <christian.reich@hicknhack-software.com>
2012-06-29 18:03:08 +0200

    added project-filter ("pj") to TimeList

--- lib/query_patch.rb
+++ lib/query_patch.rb
@@ -13,6 +13,8 @@ module QueryPatch
     base.send(:extend, ClassMethods)
     base.send(:include, InstanceMethods)
     base.class_eval do
+      alias_method_chain :available_filters, :pj
+
       base.add_available_column(QueryColumn.new(:id, :sortable => "#{TimeBooking.table_name}.id", :caption => "bookings"))
       base.add_available_column(QueryColumn.new(:time_log_id, :sortable => "#{TimeLog.table_name}.id", :caption => "logs"))
       base.add_available_column(QueryColumn.new(:time_entry_id, :sortable => "#{TimeEntry.table_name}.id", :caption => "entries"))
@@ -26,11 +28,17 @@ module QueryPatch
 
   # TODO refactor the instance methods!!
   module InstanceMethods
+
+    def available_filters_with_pj
+      @available_filters = available_filters_without_pj
+      @available_filters['pj'] = @available_filters["project_id"]
+      @available_filters
+    end
+
     # Returns the bookings count
     def booking_count
       # TODO refactor includes
-      #TimeBooking.count(:include => [:status, :project], :conditions => statement)
-      TimeBooking.count(:conditions => statement)
+      TimeBooking.count(:include => [:time_log, :time_entry], :conditions => statement)
     rescue ::ActiveRecord::StatementInvalid => e
       raise StatementInvalid.new(e.message)
     end
@@ -41,8 +49,7 @@ module QueryPatch
       if grouped?
         begin
           # Rails3 will raise an (unexpected) RecordNotFound if there's only a nil group value
-          #r = TimeBooking.count(:group => group_by_statement, :include => [:status, :project], :conditions => statement)
-          r = TimeBooking.count(:group => group_by_statement, :include => :project, :conditions => statement)
+          r = TimeBooking.count(:group => group_by_statement, :include => [:time_log, :time_entry], :conditions => statement)
         rescue ActiveRecord::RecordNotFound
           r = {nil => booking_count}
         end
@@ -67,7 +74,7 @@ module QueryPatch
       #joins = "LEFT JOIN time_entries ON #{TimeEntry.table_name}.id = #{TimeBooking.table_name}.time_entry_id"
       joins = nil
 
-      TimeBooking.scoped(:conditions => options[:conditions]).find :all, :include => ([:time_entry => :project, :time_log => [:user, :project]] + (options[:include] || [])).uniq,
+      TimeBooking.scoped(:conditions => options[:conditions]).find :all, :include => ([:time_entry => :project, :time_log => :user] + (options[:include] || [])).uniq,
                                                                    :conditions => statement,
                                                                    :order => order_option,
                                                                    :joins => joins,
@@ -79,11 +86,13 @@ module QueryPatch
     end
 
     # sql statements for where clauses have to be in an "sql_for_#{filed-name}_field" method
-    #    def sql_for_booking_comment_field(field, operator, value)
-    #db_table = TimeBooking.table_name
-    #"#{db_table}"
-    #"#{Issue.table_name}.id #{ operator == '=' ? 'IN' : 'NOT IN' } (SELECT #{db_table}.watchable_id FROM #{db_table} WHERE #{db_table}.watchable_type='Issue' AND " +
-    #    sql_for_field(field, '=', value, db_table, 'user_id') + ')'
-    #    end
+    def sql_for_pj_field(field, operator, value)
+      if value.delete('mine')
+        value += User.current.memberships.map(&:project_id).map(&:to_s)
+      end
+
+      "(#{TimeBooking.table_name}.virtual = 't' AND #{TimeLog.table_name}.project_id IN (" + value.collect { |val| "'#{connection.quote_string(val)}'" }.join(",") + ") )" +
+          " OR ( #{TimeBooking.table_name}.virtual = 'f' AND  #{TimeEntry.table_name}.project_id IN (" + value.collect { |val| "'#{connection.quote_string(val)}'" }.join(",") + ") )"
+    end
   end
 end
