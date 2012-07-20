fa23e46390aa5f5fdf8b209c39ad0bc009d0a997
Christian Reich <christian.reich@hicknhack-software.com>
2012-07-20 17:28:00 +0200

    removed unnecessary index at time_trackers_controller

--- app/controllers/time_trackers_controller.rb
+++ app/controllers/time_trackers_controller.rb
@@ -4,31 +4,6 @@ class TimeTrackersController < ApplicationController
   menu_item :time_tracker_menu_tab_overview
   before_filter :authorize_global
 
-  def index
-    @time_tracker = get_current
-
-    bookable_logs_temp = []
-    # queries with "includes" work way faster that simple user.time_logs.each
-    logs = User.current.time_logs.includes(:time_bookings).order("started_on DESC")
-    logs.each do |tl|
-      bookable_logs_temp.push(tl) if tl.bookable_hours > 0
-    end
-    # limit output of time_log list
-    # we have to prepare some values so the classic-pagination can handle or array
-    @unbooked_logs_count = bookable_logs_temp.count
-    ret = paginate_array(bookable_logs_temp, params['page_unbooked'].to_i)
-    @bookable_logs = ret[:arr]
-    @unbooked_logs_pages = Paginator.new self, @unbooked_logs_count, ret[:limit], ret[:page]
-
-    # time booking list
-    user_bookings_temp = User.current.time_bookings.includes(:virtual_comment, :time_entry => {:issue => [:status, :tracker, :priority]}).order("started_on DESC")
-    @user_bookings_count = user_bookings_temp.count
-
-    ret = paginate_array(user_bookings_temp, params['page_booked'].to_i)
-    @user_bookings = ret[:arr]
-    @booked_logs_pages = Paginator.new self, @user_bookings_count, ret[:limit], ret[:page]
-  end
-
   # we could start an empty timeTracker to track time without any association.
   # we also can give some more information, so the timeTracker could be automatically associated later.
   def start(args = {})
