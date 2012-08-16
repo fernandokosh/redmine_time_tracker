4e338f3a825fd000d511e66f69291f99e2e0efce
Christian Reich <christian.reich@hicknhack-software.com>
2012-08-16 12:58:04 +0200

    removed small glitch including the calendar-app

--- app/helpers/time_trackers_helper.rb
+++ app/helpers/time_trackers_helper.rb
@@ -40,7 +40,7 @@ module TimeTrackersHelper
   end
 
   def calendar_for_tt(field_id)
-    include_calendar_headers_tags
+    #include_calendar_headers_tags
     image_tag("calendar.png", {:id => "#{field_id}_trigger", :class => "calendar-trigger"}) +
         javascript_tag("Calendar.setup({inputField : '#{field_id}', ifFormat : '%Y-%m-%d', button : '#{field_id}_trigger', onUpdate : updateTTControllerForm });")
   end
