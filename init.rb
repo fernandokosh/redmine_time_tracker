c626239d61239341ad8cd1fdf5403ebf7f07de01
Christian Reich <christian.reich@hicknhack-software.com>
2012-10-01 16:24:35 +0200

    new version number due to porting the plug-in to redmine 2.1.x

--- init.rb
+++ init.rb
@@ -27,9 +27,9 @@ Redmine::Plugin.register :redmine_time_tracker do
   author 'Christian Reich'
   author_url 'mailto:christian.reich@hicknhack-software.com'
   description 'This is a plugin to track time in Redmine'
-  version '0.4.2'
+  version '0.5'
 
-  requires_redmine :version_or_higher => '2.0.0'
+  requires_redmine :version_or_higher => '2.1.0'
 
   #settings :default => {:refresh_rate => '60', :status_transitions => {}}, :partial => 'settings/time_tracker'
   settings :default => {:report_title => 'Report', :report_logo_url => '', :report_logo_width => '150'}, :partial => 'settings/time_tracker'
