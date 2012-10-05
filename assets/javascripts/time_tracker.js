e856868219eab75e557cd050f83a7755c996b157
creich <christian.reich@hicknhack-software.com>
2012-10-05 17:34:12 +0200

    minor code cleanup in time_tracker.js

--- assets/javascripts/time_tracker.js
+++ assets/javascripts/time_tracker.js
@@ -1,13 +1,3 @@
-/*
- * This script updates the element 'id' with 'newContent' if the two contents differ
- */
-function updateElementIfChanged(id, newContent) {
-    el = $(id);
-    if (el.innerHTML != newContent) {
-        el.update(newContent);
-    }
-}
-
 // ================== helpers for date-filter chooser ============================
 
 function get_time_span(time, obj) {
