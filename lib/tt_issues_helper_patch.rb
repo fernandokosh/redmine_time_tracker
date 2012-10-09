179151220dc53589fe4061436aeef423fd5f9b7c
creich <christian.reich@hicknhack-software.com>
2012-10-09 16:31:00 +0200

    display custom_queries on reporting page

--- lib/tt_issues_helper_patch.rb
+++ lib/tt_issues_helper_patch.rb
@@ -16,7 +16,7 @@ module IssuesHelperPatch
   module InstanceMethods
     def sidebar_queries_with_time_tracker
       sidebar_queries_without_time_tracker
-      if controller_name == "time_list"
+      if %w(tt_reporting time_list).include? controller_name
         @sidebar_queries.delete_if { |item| !item.tt_query? }
       else
         @sidebar_queries.delete_if { |item| item.tt_query? }
