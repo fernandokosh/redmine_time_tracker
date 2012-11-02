95d2c00e6227033ec1893f1d4e00572f42c87c1a
creich <christian.reich@hicknhack-software.com>
2012-11-02 15:20:59 +0100

    fixed time_booking dependencies (deletion of time_entry also destroys timeBooking) minor code-cleanup in tt_bookings_list => _list

--- app/models/time_booking.rb
+++ app/models/time_booking.rb
@@ -4,7 +4,7 @@ class TimeBooking < ActiveRecord::Base
   attr_accessible :started_on, :stopped_at, :time_entry_id, :time_log_id, :project, :project_id
   belongs_to :project
   belongs_to :time_log
-  belongs_to :time_entry
+  belongs_to :time_entry, :dependent =>  :delete
   has_one :issue, :through => :time_entry
 
   validates_presence_of :time_log_id
