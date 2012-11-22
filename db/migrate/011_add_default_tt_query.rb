7d40973a2c8057ab29ef181717d00fc3b2a3f137
creich <christian.reich@hicknhack-software.com>
2012-11-22 11:03:49 +0100

    removed outdated code

--- db/migrate/011_add_default_tt_query.rb
+++ db/migrate/011_add_default_tt_query.rb
@@ -1,16 +1,10 @@
 class AddDefaultTtQuery < ActiveRecord::Migration
+
+  # broken during commit 9a71a1a2e51f2e9cf2436c5f397400b998188073
+  # code is not really necessary, because it only creates some default queries.
   def up
-    Query.create :tt_query => true,
-                 :is_public => true,
-                 :name => I18n.t(:time_tracker_label_your_time_bookings),
-                 :filters => {:tt_user => {:operator => "=", :values => ["me"]}},
-                 :group_by => nil,
-                 :column_names => [:project, :tt_booking_date, :get_formatted_start_time, :get_formatted_stop_time, :issue, :comments, :get_formatted_time]
   end
 
   def down
-    query = Query.where(:tt_query => true,
-                        :name => I18n.t(:time_tracker_label_your_time_bookings)).first # should be the first tt_query in the db..
-    Query.destroy(query.id) unless query.nil?
   end
 end
