e865296d412e12250bfc06bb29cfbbac09d00d14
Robert Kranz <robert.kranz@hicknhack-software.com>
2013-06-24 16:21:47 +0200

    fixed #43 *Activity stays in redmine, if there are still entries refering to

--- db/migrate/012_add_tt_activity.rb
+++ db/migrate/012_add_tt_activity.rb
@@ -7,5 +7,7 @@ class AddTtActivity < ActiveRecord::Migration
   def down
     tea = TimeEntryActivity.where(:name => :time_tracker_activity).first
     TimeEntryActivity.destroy(tea.id) unless tea.nil?
+  rescue
+    Rails.logger.info "Can't delete the TimeEntryActivity. There are still redmine activities with this booked."
   end
 end
