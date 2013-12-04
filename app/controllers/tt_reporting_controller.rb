2c4a1e38fdfdfae53e6e52a15a3d85944f78b9d7
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2013-12-04 17:18:25 +0100

    * fixed external logo support * fixed reporting_controller class * added start and stop time to report * added issue milestone to report * removed link from report * improved report print styling * improved localisations

--- app/controllers/tt_reporting_controller.rb
+++ app/controllers/tt_reporting_controller.rb
@@ -38,6 +38,9 @@ class TtReportingController < ApplicationController
     render_404
   end
 
+  WWW_START_REGEXP = /^www/
+  HTTP_START_REGEXP = /^http[s]?:\/\//
+
   def print_report
     fetch_query
 
@@ -52,66 +55,67 @@ class TtReportingController < ApplicationController
 
     # prepare logo settings
     @logo_url = Setting.plugin_redmine_time_tracker[:report_logo_url]
-    @logo_url = "http://#{@logo_url}" if Regexp.new(/^www/).match(@logo_url)
-    @logo_url.start_with?("http://") ? @logo_external = true : @logo_external = false
-    Setting.plugin_redmine_time_tracker[:report_logo_width].blank? ? @logo_width = "150" : @logo_width = Setting.plugin_redmine_time_tracker[:report_logo_width]
+    @logo_url = "http://#{@logo_url}" if @logo_url =~ WWW_START_REGEXP
+    @logo_external = @logo_url =~ HTTP_START_REGEXP ? true : false
+    @logo_width = Setting.plugin_redmine_time_tracker[:report_logo_width]
+    @logo_width = "150" if @logo_width.blank?
 
     render "print_report", :layout => false, :locals => {:query => @query_bookings}
   end
-end
 
-def total_booked
-  hours = 0
-  @bookings.each do |tb|
-    hours += tb.hours_spent
-  end
-  hours
-end
+  private
 
-private
-
-def fetch_query
-  query_from_id
-  time_bookings_query
-
-  unless help.permission_checker([:tt_view_bookings, :tt_edit_bookings], {}, true)
-    @query_bookings.filters[:tt_user] = {:operator => "=", :values => [User.current.id.to_s]}
+  def total_booked
+    hours = 0
+    @bookings.each do |tb|
+      hours += tb.hours_spent
+    end
+    hours
   end
 
-  sort_init(@query_bookings.sort_criteria.empty? ? [['tt_booking_date', 'desc']] : @query_bookings.sort_criteria)
-  tt_sort_update(:sort_bookings, @query_bookings.sortable_columns, "tt_booking_sort")
-end
+  def fetch_query
+    query_from_id
+    time_bookings_query
 
-def fetch_chart_data
-  @chart_data = Array.new
-  @chart_ticks = Array.new
-  @highlighter_data = Array.new
-  
-  if @query_bookings.valid? && !(@bookings.empty? || @bookings.nil?)
-    # if the user changes the date-order for the table values, we have to reorder it for the chart
-    start_date = [@bookings.last.started_on.to_date, @bookings.first.started_on.to_date].min
-    stop_date = [@bookings.last.started_on.to_date, @bookings.first.started_on.to_date].max
+    unless help.permission_checker([:tt_view_bookings, :tt_edit_bookings], {}, true)
+      @query_bookings.filters[:tt_user] = {:operator => "=", :values => [User.current.id.to_s]}
+    end
 
-    (start_date..stop_date).map do |date|
-      hours = 0
-      @bookings.each do |tb|
-        hours += tb.hours_spent if tb.started_on.to_date == date
-      end
-      @chart_data.push(hours)
-      @highlighter_data.push([date, hours])
+    sort_init(@query_bookings.sort_criteria.empty? ? [['tt_booking_date', 'desc']] : @query_bookings.sort_criteria)
+    tt_sort_update(:sort_bookings, @query_bookings.sortable_columns, "tt_booking_sort")
+  end
 
-      # to get readable labels, we have to blank out some of them if there are to many
-      # only set 8 labels and set the other blank
-      gap = ((stop_date - start_date)/8).ceil
-      if gap == 0 || (date - start_date) % gap == 0
-        @chart_ticks.push(date)
-      else
-        @chart_ticks.push("")
+  def fetch_chart_data
+    @chart_data = Array.new
+    @chart_ticks = Array.new
+    @highlighter_data = Array.new
+
+    if @query_bookings.valid? && !(@bookings.empty? || @bookings.nil?)
+      # if the user changes the date-order for the table values, we have to reorder it for the chart
+      start_date = [@bookings.last.started_on.to_date, @bookings.first.started_on.to_date].min
+      stop_date = [@bookings.last.started_on.to_date, @bookings.first.started_on.to_date].max
+
+      (start_date..stop_date).map do |date|
+        hours = 0
+        @bookings.each do |tb|
+          hours += tb.hours_spent if tb.started_on.to_date == date
+        end
+        @chart_data.push(hours)
+        @highlighter_data.push([date, hours])
+
+        # to get readable labels, we have to blank out some of them if there are to many
+        # only set 8 labels and set the other blank
+        gap = ((stop_date - start_date)/8).ceil
+        if gap == 0 || (date - start_date) % gap == 0
+          @chart_ticks.push(date)
+        else
+          @chart_ticks.push("")
+        end
       end
     end
   end
-end
 
-def check_settings_for_ajax
-  flash[:error] = l(:force_auth_requires_rest_api) if Setting.login_required? && !Setting.rest_api_enabled?
+  def check_settings_for_ajax
+    flash[:error] = l(:force_auth_requires_rest_api) if Setting.login_required? && !Setting.rest_api_enabled?
+  end
 end
