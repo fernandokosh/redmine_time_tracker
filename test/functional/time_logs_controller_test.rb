328e3710ee53c91ce9856f09ab46d7f6ab92e48b
Lars Beier <lars.beier@hicknhack-software.com>
2014-04-23 17:03:15 +0200

    Undo require test_helper in test suites

--- test/functional/time_logs_controller_test.rb
+++ test/functional/time_logs_controller_test.rb
@@ -1,4 +1,5 @@
-require_relative '../test_helper'
+require File.dirname(__FILE__) + '../../test_helper'
+
 
 class TimeLogsControllerTest < ActionController::TestCase
   fixtures :projects, :users, :user_preferences, :roles, :members, :member_roles, :issues, :trackers, :issue_statuses, :enabled_modules,
