c55ee2830c944f04b86f3f2a6613470f289c6152
Lars Beier <lars.beier@hicknhack-software.com>
2014-04-30 14:30:50 +0200

    Change capybara integration test.

--- test/integration/time_tracker_integration_test.rb
+++ test/integration/time_tracker_integration_test.rb
@@ -3,21 +3,21 @@ require File.dirname(__FILE__) + '../../minitest_helper'
 
 class TimeTrackerIntegrationTest < RedmineTimeTracker::IntegrationTest
 
-  context 'Time tracker integration' do
+  context 'User' do
 
     def setup
       log_user('jsmith', 'jsmith') 
       # current user is John Smith (no admin) in each test of this context
       # set permissions for this user
+      Role.find(2).add_permission! :tt_log_time
     end
 
 
-    should "have permission to visit time tracker overview page" do
-      Role.find(2).add_permission! :tt_log_time
+    should "have permission to start a time tracker", js: true do
       visit '/tt_overview'
-
       page.text.must_include 'Your time logs'
+      click_button('start')
+      page.text.must_include 'Started the time tracker'
     end
   end
 end
-
