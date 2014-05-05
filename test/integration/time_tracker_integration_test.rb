335394e19811c1da8a1082cb03a0856a6c6bb163
Lars Beier <lars.beier@hicknhack-software.com>
2014-05-05 09:25:11 +0200

    Change click_button to find and click.

--- test/integration/time_tracker_integration_test.rb
+++ test/integration/time_tracker_integration_test.rb
@@ -17,23 +17,23 @@ class TimeTrackerIntegrationTest < RedmineTimeTracker::IntegrationTest
     should "have permission to start a time tracker" do
       visit '/tt_overview'
       page.text.must_include 'Your time logs'
-      click_button('start-time-tracker-button')
+      find('#start-time-tracker-button').click
       page.text.must_include 'Started the time tracker'
-      click_button('stop-time-tracker-button')
+      find('#stop-time-tracker-button').click
     end
 
     should "have started one time tracker" do
       visit '/tt_overview'
-      click_button('start-time-tracker-button')
+      find('#start-time-tracker-button').click
       time_tracker = TimeTracker.where(user_id: User.current.id)
       assert_equal(time_tracker.exists?, true)
-      click_button('stop-time-tracker-button')
+      find('#stop-time-tracker-button').click
     end
 
     should "have created a time log after stopping the time tracker" do
       visit '/tt_overview'
-      click_button('start-time-tracker-button')
-      click_button('stop-time-tracker-button')
+      find('#start-time-tracker-button').click
+      find('#stop-time-tracker-button').click
       page.text.must_include 'Stopped the time tracker'
       time_logs = TimeLog.where(user_id: User.current.id)
       assert_equal(time_logs.size, 1)
