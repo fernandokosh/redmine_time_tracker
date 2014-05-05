3a82a57bc9d907d589f7650f7ca519817af5c03f
Lars Beier <lars.beier@hicknhack-software.com>
2014-05-05 09:09:38 +0200

    Add css id to time tracker start and stop button for identification.

--- test/integration/time_tracker_integration_test.rb
+++ test/integration/time_tracker_integration_test.rb
@@ -17,23 +17,23 @@ class TimeTrackerIntegrationTest < RedmineTimeTracker::IntegrationTest
     should "have permission to start a time tracker" do
       visit '/tt_overview'
       page.text.must_include 'Your time logs'
-      click_button('start')
+      click_button('start-time-tracker-button')
       page.text.must_include 'Started the time tracker'
-      click_button('stop')
+      click_button('stop-time-tracker-button')
     end
 
     should "have started one time tracker" do
       visit '/tt_overview'
-      click_button('start')
+      click_button('start-time-tracker-button')
       time_tracker = TimeTracker.where(user_id: User.current.id)
       assert_equal(time_tracker.exists?, true)
-      click_button('stop')
+      click_button('stop-time-tracker-button')
     end
 
     should "have created a time log after stopping the time tracker" do
       visit '/tt_overview'
-      click_button('start')
-      click_button('stop')
+      click_button('start-time-tracker-button')
+      click_button('stop-time-tracker-button')
       page.text.must_include 'Stopped the time tracker'
       time_logs = TimeLog.where(user_id: User.current.id)
       assert_equal(time_logs.size, 1)
